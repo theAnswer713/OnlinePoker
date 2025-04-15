ecbe94540c89b605ab86bcb469a991c375cc95b8
Eggsandwichman <e.jimmothy@gmail.com>
2025-04-15 07:26:00 -0500

    Add files via upload

---
+++ Chip.java
@@ -0,0 +1,11 @@
+public class Chip {
+    private int value;
+    
+    public Chip(int value) {
+        this.value = value;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+}
