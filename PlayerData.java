4440616eff143f912932592eac3973bb695ae7d7
theAnswer713 <108958871+theAnswer713@users.noreply.github.com>
2025-04-15 07:44:24 -0500

    Add files via upload

--- PlayerData.java
+++ PlayerData.java
@@ -1,2 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class PlayerData {
+    private Socket socket;
+    private String username;
+    private BufferedReader br;
+    private PrintWriter pw;
+
+    public PlayerData(Socket socket, String username) throws Exception {
+        this.socket = socket;
+        this.username = username;
+        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        pw = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public BufferedReader getBr() {
+        return br;
+    }
+    public PrintWriter getPw() {
+        return pw;
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.Set;
@@ -24,7 +26,23 @@ public class Server {
         public void run() {
             try {
                 while(!server.isClosed()) {
+                    while(playerSet.size()<4) {
+                        System.out.println("Waiting for players to join...");
+                        Socket socket = server.accept();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String username = br.readLine();
+                        PlayerData pd = new PlayerData(socket,username);
+                        playerSet.add(pd);
 
+                        Thread listenThread = new Thread(new ListenThread(pd));
+                        listenThread.start();
+
+                        String message = "JOIN:"+username;
+                        System.out.println(message);
+                        for(PlayerData x:playerSet) {
+                            x.getPw().println(message);
+                        }
+                    }
                 }
             }
             catch(Exception err) {
@@ -34,9 +52,17 @@ public class Server {
     }
 
     private class ListenThread implements Runnable {
+        private PlayerData pd;
+
+        public ListenThread(PlayerData pd) {
+            this.pd = pd;
+        }
+
         public void run() {
             try {
-
+                while(!pd.getSocket().isClosed()) {
+                    String message = pd.getBr().readLine();
+                }
             }
             catch(Exception err) {
 
