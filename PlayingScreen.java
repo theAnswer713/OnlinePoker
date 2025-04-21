3154000194403f237fb0d5ca919fcbe7fa48e13a
theAnswer713 <108958871+theAnswer713@users.noreply.github.com>
2025-04-20 19:03:40 -0500

    Add files via upload

--- PlayingScreen.java
+++ PlayingScreen.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -20,5 +21,38 @@ public class PlayingScreen {
         }
 
         JFrame frame = new JFrame("Poker");
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new GridBagLayout());
+        //where the game is shown and players are
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new GridBagLayout());
+        //where the action buttons are and player's cards
+
+        //create buttons that appear under certain conditions
+        //make buttons have functions that send certain messages
+        //code the messages to start with unique identifiers
+        //determine what those identifiers will be
+
+        frame.add(centerPanel, BorderLayout.CENTER);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    private class ListenThread implements Runnable {
+        public void run() {
+            try{
+                while(!socket.isClosed()) {
+                    String message = br.readLine();
+                    if(message.startsWith("JOIN")) {
+                        //add players in
+                    }
+                    //add else ifs with different beginnings of a message
+                }
+            }
+            catch(Exception err) {
+                err.printStackTrace();
+            }
+        }
     }
 }
