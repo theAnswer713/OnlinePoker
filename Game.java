a55dedeec30f49f2393f2aef7a92997c57893bf2
Eggsandwichman <e.jimmothy@gmail.com>
2025-04-23 08:05:55 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
-import java.util.TreeMap;
+import java.util.HashMap;
 
 public class Game {
     private ArrayList<Player> players;
     private Deck deck;
     private ArrayList<Player> winners;
+    private HashMap<String, Integer> hands;
+    private HashMap<String, Integer> highBets;
+    private HashMap<String, Integer> bets;
+    private int pot;
     
     public Game(ArrayList<Player> players) {
         this.players = players;
         winners = new ArrayList<Player>();
         deck = new Deck();
+        hands = new HashMap();
+        highBets = new HashMap();
+        bets = new HashMap();
+        pot = 0;
         for(int i=0; i<10; i++) {
             deck.Shuffle();
         }
@@ -38,7 +46,7 @@ public class Game {
             }
             match = true;
             for(int i=0; i<players.size(); i++) {
-                if(players.get(i).isFolded()==false && players.get(i).highBet()!=highest) {
+                if(players.get(i).isFolded()==false && highBets.get(players.get(i).getName())==highest) {
                     match = false;
                 }
             }
@@ -76,15 +84,35 @@ public class Game {
     
     //compare
     public void compare() {
-        TreeMap<String, Integer> hands = new TreeMap();
         for(int i=0; i<players.size(); i++) {
             if(players.get(i).isFolded()==false) {
                 hands.put(players.get(i).getName(), players.get(i).handValue());
             }
         }
     }
+    public String bestHand() {
+        int best = 0;
+        String bestName = new String("");
+        for(int i=0; i<players.size(); i++) {
+            if(hands.get(players.get(i).getName()) > best) {
+                best = hands.get(players.get(i).getName());
+                bestName = players.get(i).getName();
+            }
+            else if(hands.get(players.get(i).getName()) == best) {
+                bestName = bestName+" and "+players.get(i).getName();
+            }
+        }
+        return bestName;
+    }
     //money to winner
     public void distribute() {
-        
+        int remainder = 0;
+        while(pot>0) {
+            String bestName = this.bestHand();
+            
+            for(int i=0; i<players.size(); i++) {
+                int temp = Math.min(bets.get(players.get(i).getName()), 
+            }
+        }
     }
 }
